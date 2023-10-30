fb237efe6405ff33b726bf67f0101440f09ee15d
Florian Kreuk <fkreuk@cartermario.com>
2023-10-30 10:36:09 -0400

    added route for single category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -19,10 +19,26 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-});
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: {
+        model: Product,
+        attributes: ['product_name'],
+      },
+    });
+    if (categoryData) {
+      res.status(200).json(categoryData);
+    } else {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+    } catch {
+      res.status(500).json(err);
+    }
+  });
 
 router.post('/', (req, res) => {
   // create a new category
