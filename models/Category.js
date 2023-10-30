9089745d294376504b67704349c287b3761af611
Florian Kreuk <fkreuk@cartermario.com>
2023-10-30 09:36:45 -0400

    added the column definitions for Category in model file

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,17 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // defined columns for id and category name
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
