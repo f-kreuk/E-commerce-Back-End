e32a149141210602320d1e7b6e96321b482f9c9f
Florian Kreuk <fkreuk@cartermario.com>
2023-10-30 10:48:58 -0400

    added post route for a new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,13 +7,13 @@ router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const categories = await Category.findAll({
+    const categoryData = await Category.findAll({
       include: { 
         model: Product,
         attributes: ['product_name'] 
       }
     });
-    res.status(200).json(categories);
+    res.status(200).json(categoryData);
   } catch {
     res.status(500).json(err);
   }
@@ -40,8 +40,15 @@ router.get('/:id', async (req, res) => {
     }
   });
 
-router.post('/', (req, res) => {
+
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
